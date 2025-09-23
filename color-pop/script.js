66608072668829551afff002f8a97350990f98be
Watcher At The Bridge <monahanhunt@gmail.com>
2025-09-23 08:41:29 -0400

    Update script.js

--- color-pop/script.js
+++ color-pop/script.js
@@ -529,7 +529,7 @@ class ColorPopGame {
        // if (this.audioSystem && this.audioSystem.isReady()) {
            // this.audioSystem.playVoicePrompt('great-job');
           //  this.audioSystem.playSound('pop');
-        }
+       // }
         
         // Show success message
         this.showFeedbackMessage('Great job! 🎉', 'success');
